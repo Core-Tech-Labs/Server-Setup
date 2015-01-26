cb98924fa75b67fa32499c744709e5cc0b31aa83
Rudy Jessop <rudyjessop@gmail.com>
2015-01-26 12:46:40 -0500

    implementing git and composer

--- setup.sh
+++ setup.sh
@@ -16,19 +16,52 @@ apt-get install git -y
 
 echo "Updating PHP repo"
 apt-get install python-software-properties build-essential -y 
-add-apt-repository ppa:ondrej/php5-oldstable -y  #add-apt-repository ppa:ondrej/php5-oldstable research add-apt-repository ppa:ondrej/php5
+add-apt-repository ppa:ondrej/php5 -y  
 apt-get update
+
+echo "Install Git"
+apt-get install git 
+
+echo ""
 
 echo "Installing PHP"
 apt-get install php5-common php5-dev php5-cli php5-fpm -y 
 
 echo "Installing PHP extensions"
-apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y 
+apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y php5-xdebug php5-gd php5-readline libapache2-mod-php5 
+
+cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
+xdebug.scream=1
+xdebug.cli_color=1
+xdebug.show_local_vars=1
+EOF
+
+echo "Enable Apache2 mod-rewrite"
+a2emod rewrite
+
+echo "Set Web Root"
+
+
+
+
+
+echo "Installing MySQL"
+apt-get install mysql-server -y 
 
 echo "Preparing MySQL"
 apt-get install debconf-utils -y 
 debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
 
-echo "Installing MySQL"
-apt-get install mysql-server -y 
+echo ".....Restarting Apache"
+service apache2 Restart
+
+echo "Installing the General....Composer"
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+
+echo "To be Contiune..."
+
+
+
